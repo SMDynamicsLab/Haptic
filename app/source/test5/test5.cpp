d3a3ad3ba57adaaf3eaf1e65ddc8c1588a0dc097
Carolina Fritz <carolinafritz1996@gmail.com>
2021-08-27 18:50:19 -0300

    getVariables (parser) habilitado en test5

--- app/source/test5/test5.cpp
+++ app/source/test5/test5.cpp
@@ -92,7 +92,7 @@ int main(int argc, char* argv[])
     cout << "Input file is "<< input << endl;
     cout << "Output file is "<< output << endl;
     
-    // getVariables(input, mod_time, variables);
+    getVariables(input, mod_time, variables);
 
     // OPEN GL - WINDOW DISPLAY
     // initialize GLFW library
