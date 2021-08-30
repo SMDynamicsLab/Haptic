77c5a64b497ff8ac9076a6269e5dc6c548a8105f
Carolina Fritz <carolinafritz1996@gmail.com>
2021-08-30 16:32:16 -0300

    Cambios en la estructura del script de python

--- app/source/test5/run.py
+++ app/source/test5/run.py
@@ -3,35 +3,74 @@ import subprocess
 import pathlib
 import time
 import sys
-status, output = subprocess.getstatusoutput('make')
-if status != 0:
-    print(output)
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
 
-data_path = os.path.join(sys.path[0], 'data')
-os.makedirs(data_path, exist_ok=True)
+def run_make():
+    p_status, p_output = subprocess.getstatusoutput('make')
+    if p_status != 0:
+        print(p_output)
 
-input = os.path.join(data_path, 'in.csv')
-output = os.path.join(data_path, 'out.csv')
+def start_simulation(bin_file, input, output):
+    if not os.path.isfile(bin_file):
+        print(f'Bin file does not exist at {bin_file}')
 
-# agregar chequeo sobre si existe
-fname = pathlib.Path(input)
-input_mod_time = fname.stat().st_mtime # epoch float
+    subprocess.Popen([
+        bin_file, 
+        input, 
+        output
+        ]
+        # , stdout=subprocess.DEVNULL #para evitar que salga a consola
+        )
 
-bin_file = os.path.join( sys.path[0], '../../bin/lin-x86_64/test5')
-subprocess.Popen([
-    bin_file, 
-    input, 
-    output
-    ]
-    # , stdout=subprocess.DEVNULL #para evitar que salga a consola
-    )
+def plot_trials(output):
+    # estructura trial, x, y, z
+    names = ['trial', 'x', 'y', 'z']
+    df = pd.read_csv(output, names=names)
+    x_min = df['x'].min()
+    df['x'] = df['x']-x_min
+    fig, ax = plt.subplots()
+    for trial, group in df.groupby('trial'):
+        group.plot(x='x', y='y', ax=ax, label=f'trial {trial}')
+    plt.show()
+    print(df.head)
+    return
 
-# Consumo de memoria/CPU: htop -p "$(pgrep -d , "python|test")"
-while True:
-    # no sleep 99% CPU
-    # time.sleep(0.001) #~9% CPU
-    time.sleep(0.01) # ~2% CPU
-    if input_mod_time != fname.stat().st_mtime:
-        print('file changed')
-        input_mod_time = fname.stat().st_mtime
+def change_variables(input):
+    print(input)
+    return
+
+def start_controller(input, output):
+    fname = pathlib.Path(output)
+    output_mod_time = None # epoch float
+
+    # Consumo de memoria/CPU: htop -p "$(pgrep -d , "python|test")"
+    while True:
+        # no sleep 99% CPU
+        # time.sleep(0.001) #~9% CPU
+        time.sleep(0.01) # ~2% CPU
+        if os.path.isfile(output):
+            mod_time = fname.stat().st_mtime 
+            if output_mod_time != mod_time:
+                print('file changed')
+                output_mod_time = mod_time
+                plot_trials(output)
+                change_variables(input)
+
+if __name__ == "__main__":
+    run_make()
+
+    data_path = os.path.join(sys.path[0], 'data')
+    os.makedirs(data_path, exist_ok=True)
+    input = os.path.join(data_path, 'in.csv')
+    output = os.path.join(data_path, 'out.csv')
+
+    bin_file = os.path.join( sys.path[0], '../../bin/lin-x86_64/test6')
+
+    start_simulation(bin_file, input, output)
+    start_controller(input, output)
+
+
+
 
