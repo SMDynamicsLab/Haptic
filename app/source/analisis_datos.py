9eb36ad271ed985982f653b3589f20a1cd1188e5
Carolina Fritz <carolinafritz1996@gmail.com>
2022-06-25 08:12:33 -0300

    Datos vft completos

---
+++ app/source/analisis_datos.py
@@ -0,0 +1,13 @@
+plot(output_file, plot_file)
+
+
+vft_agusbarreto = "vft_agusbarreto_"
+vft_ayelensantos = "vft_ayelensantos_"
+vft_caro = "vft_caro_"
+vft_constanza = "vft_constanza_"
+vft_gabrielgoren = "vft_gabrielgoren_"
+vft_hilariob = "vft_hilariob_"
+vft_luciabusolini = "vft_luciabusolini_"
+vft_matiasherrera = "vft_matiasherrera_"
+vft_melisavinograd = "vft_melisavinograd_"
+vft_yamila = "vft_yamila_"
