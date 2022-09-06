24601d4c7e76a1c81ba363ec723f730efae1e86c
Carolina Fritz <carolinafritz1996@gmail.com>
2022-09-06 14:32:59 -0300

    Nuevas imagenes

--- app/source/plot/distance.py
+++ app/source/plot/distance.py
@@ -10,6 +10,13 @@ def distance(data):
     d = np.sqrt(ds.x ** 2 + ds.y ** 2)
     return d.sum()
 
+def distance_vect(data):
+    df = data.reset_index(drop=True)
+    p1 = df.iloc[:-1][['x','y']]
+    p2 = df.iloc[1:][['x','y']]
+    p2 = p2.reset_index(drop=True)
+    ds = p2 - p1  
+    return ds  
 
 def total_distance(data):
     df = data.reset_index(drop=True)
